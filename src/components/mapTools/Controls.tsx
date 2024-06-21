dede0579dc45f90a1bc04664085251376318dfdc
HullPerse <alex31054@yandex.ru>
2024-06-22 02:26:57 +0300

    ADDED: load from json

--- src/components/mapTools/Controls.tsx
+++ src/components/mapTools/Controls.tsx
@@ -1,22 +1,88 @@
 import { useControls } from "react-zoom-pan-pinch";
 import { Button } from "@/components/ui/button";
 import { Input } from "../ui/input";
-import { useState } from "react";
+import { useContext, useState } from "react";
+import { MapContext } from "@/provider/hexMapProvider";
+
+interface HexBrush {
+  id: string;
+  title: string;
+  color: string;
+  clickable: boolean;
+}
+
+interface CustomHex {
+  id: string;
+  type: HexBrush;
+}
 
 export default function Controls() {
   const { resetTransform } = useControls();
 
+  const { customHex, setCustomHex } = useContext(MapContext);
+
   const [isDownloading, setIsDownloading] = useState(false);
+
+  const handleDownload = () => {
+    setIsDownloading(true);
+
+    const jsonString = `data:text/json;chatset=utf-8,${encodeURIComponent(
+      JSON.stringify({ ...customHex })
+    )}`;
+    const link = document.createElement("a");
+    link.href = jsonString;
+    link.download = "map.json";
+    link.click();
+
+    setIsDownloading(false);
+    return;
+  };
+
+  const handleUpload = () => {
+    const input = document.getElementById("loadjson");
+    if (!input) return;
+
+    input.onchange = () => {
+      //@ts-expect-error next-line
+      if (input.files.length <= 0) return;
+
+      const reader = new FileReader(); // File reader to read the file
+
+      // This event listener will happen when the reader has read the file
+      reader.onload = () => {
+        const result = reader.result; // Parse the result into an object
+        if (!result) return;
+        const parsedResult = JSON.parse(result as string);
+
+        const hexArray: CustomHex[] = [];
+
+        Object.entries(parsedResult).forEach(([, hex]) => {
+          hexArray.push({ ...(hex as CustomHex) });
+        });
+
+        setCustomHex(hexArray);
+        localStorage.setItem("customHex", JSON.stringify(hexArray));
+        reader.abort();
+      };
+
+      //@ts-expect-error next-line
+      reader.readAsText(input.files[0]);
+    };
+  };
 
   return (
     <section className="flex flex-col w-full h-full items-center justify-end gap-2 ">
       <div className="inline-flex w-full gap-2 px-2">
-        <Input id="loadjson" type="file" className="hidden" accept=".json" />
+        <Input
+          id="loadjson"
+          type="file"
+          className="hidden"
+          accept=".json"
+          onInput={handleUpload}
+        />
         <Button
           className="w-full"
-          onClick={() => {
-            alert("Not implemented yet");
-          }}
+          onClick={handleDownload}
           disabled={isDownloading}
         >
           Save JSON
